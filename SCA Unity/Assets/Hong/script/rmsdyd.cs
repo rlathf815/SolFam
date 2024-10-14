c84f8c37ff87e1827ceeea41724212ce59cb36d0
MINHONGGI1223 <push1245@gmail.com>
2024-10-14 19:43:12 +0900

    근용쌤 1일차

--- SCA Unity/Assets/Hong/script/rmsdyd.cs
+++ SCA Unity/Assets/Hong/script/rmsdyd.cs
@@ -1,18 +1,27 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class rmsdyd : MonoBehaviour
 {
+    public Transform player;
+    public float followDistance = 5.0f;
+    public float moveSpeed = 3.0f;
+    public float viewAngle = 45.0f;
     // Start is called before the first frame update
-    void Start()
+    private void Update()
     {
-        
-    }
+        float distance = Vector3.Distance(transform.position, player.position);
 
-    // Update is called once per frame
-    void Update()
-    {
-        
+        Vector3 diretictionToPlayer = (player.position - transform.position).normalized;
+
+        float angle = Vector3.Angle(transform.forward, diretictionToPlayer);
+
+        if (distance < followDistance && angle < viewAngle)
+        {
+            transform.position += diretictionToPlayer * moveSpeed * Time.deltaTime;
+        }
+        transform.LookAt(player);
     }
 }
