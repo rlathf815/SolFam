51d66dafd0fc5279c27c01bb4bf6029d2dafbc9b
limeblue517 <zoonpark123@naver.com>
2024-10-16 19:24:05 +0900

    Script Update

--- SCA Unity/Assets/Script/Nam_yeppy.cs
+++ SCA Unity/Assets/Script/Nam_yeppy.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting.FullSerializer;
 using UnityEngine;
 using UnityEngine.AI;
 
@@ -7,6 +8,9 @@ public class Nam_yeppy : MonoBehaviour
 {
     public Transform player;
     private NavMeshAgent agent;
+    RaycastHit hit;
+    bool canmove;
+    public Transform home;
     void Start()
     {
         agent = GetComponent<NavMeshAgent>();
@@ -14,6 +18,24 @@ public class Nam_yeppy : MonoBehaviour
     }
     void Update()
     {
-        agent.destination = player.position;
+        if (canmove == true)
+        {
+            agent.destination = player.position;
+        }
+        if (Physics.SphereCast(transform.position, 2.5f, transform.forward, out hit, 20)&&canmove==false)
+        {
+            if(hit.transform.gameObject.name=="FPC")
+            {
+                canmove = true;
+            }
+        }
+    }
+    void OnTriggerEnter(Collider coll)
+    {
+        if (coll.name=="FPC"&&canmove == true)
+        {
+            canmove = false;
+            agent.destination = home.position;
+        }
     }
 }
