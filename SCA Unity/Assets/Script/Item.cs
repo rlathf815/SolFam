fce3867141ba72d7a8e845a27375d495bbf25386
rlathf815 <101699987+rlathf815@users.noreply.github.com>
2024-11-08 19:47:29 +0900

    인벤토리

--- SCA Unity/Assets/Script/Item.cs
+++ SCA Unity/Assets/Script/Item.cs
@@ -1,16 +1,17 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Playables;
 
 public class Item : MonoBehaviour
 {
-    // 아이템이 획득될 때 호출되는 메서드
+    public GameData gameData;
     public void PickUp()
     {
-        // 아이템 획득 로직을 여기에 작성
         Debug.Log($"{gameObject.name} has been picked up!");
 
-        // 아이템을 획득한 후 사라지게 함
+        //gameData.AddItem(gameObject.name);
         Destroy(gameObject);
     }
+
 }
