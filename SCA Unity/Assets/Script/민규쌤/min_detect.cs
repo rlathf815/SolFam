61ac64d6c990ef73fd8e11ffc28195bf5a691414
limeblue517 <zoonpark123@naver.com>
2024-11-22 19:27:11 +0900

    error

--- SCA Unity/Assets/Script/민규쌤/min_detect.cs
+++ SCA Unity/Assets/Script/민규쌤/min_detect.cs
@@ -4,14 +4,29 @@ using UnityEngine;
 
 public class min_detect : MonoBehaviour
 {
+    bool detect = false;
     private Animator ani;
     void Start()
     {
         ani=GetComponent<Animator>();
     }
+    void Update()
+    {
+        if (detect == true)
+        {
+            FirstPersonMovement.speed = 0f;
+            yeppy_player.catched = true;
+            ani.setBool("isHello",true);
+            StartCoroutine(WaitAni);
+        }
+    }
+    IEnumerator WaitAni()
+    {
+        yield return new WaitForSeconds(4.067f);
+        ani.setBool("isHello", false);
+    }
     void OnTriggerEnter(Collider col)
     {
-        ani.SetTrigger("isHello");
-        FirstPersonMovement.speed = 0f;
+        detect = true;
     }
 }
