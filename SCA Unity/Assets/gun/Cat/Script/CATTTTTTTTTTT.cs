d97798b576724419c9a031ad4a5b1d2ccccc0f2d
rhrjs555 <kogeon1213@gmail.com>
2025-01-13 19:41:51 +0900

    애니메이션 초반

--- SCA Unity/Assets/gun/Cat/Script/CATTTTTTTTTTT.cs
+++ SCA Unity/Assets/gun/Cat/Script/CATTTTTTTTTTT.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
+using UnityEditor.Animations;
 using UnityEngine;
 
 public class CATTTTTTTTTTT : MonoBehaviour
@@ -11,18 +12,21 @@ public class CATTTTTTTTTTT : MonoBehaviour
     private bool isFollowing = false; // 현재 따라오고 있는지 여부
     private Renderer objectRenderer; // 오브젝트의 Renderer
     private bool isClose;
-
+    private Animation Animator;
+    public GameObject cat;
+    public Animator controller;
     void Start()
     {
         isClose = false;
         objectRenderer = GetComponent<Renderer>();
         objectRenderer.enabled = true; // 처음에는 보이도록 설정
+        Animator = GetComponent<Animation>();
     }
 
     void Update()
     {
         // E 키를 눌렀을 때
-        if (Input.GetKeyDown(KeyCode.E))
+        if (isClose && Input.GetKeyDown(KeyCode.E))
         {
             // isFollowing이 true일 경우 반응하지 않음
             if (isFollowing)
@@ -31,10 +35,11 @@ public class CATTTTTTTTTTT : MonoBehaviour
             }
 
             // 30% 확률로 따라오기
-            if (Random.value <= 0.3f) // 30% 확률
+            if (Random.value <= 0.9f) // 30% 확률
             {
                 isFollowing = true;
                 Debug.Log("Following the player.");
+                controller.SetTrigger("Follow");
             }
             else
             {
@@ -47,21 +52,32 @@ public class CATTTTTTTTTTT : MonoBehaviour
         // 따라오기
         if (isFollowing)
         {
+            text.SetActive(false);
             Follow();
+            if (Input.GetKeyDown(KeyCode.W))
+            {
+                Animator.SetBool("Walk", true);
+                controller.SetBool("Stop", false);
+            }
+            else
+            {
+                controller.SetBool("Walk", false);
+                controller.SetBool("Stop", true);
+            }
         }
     }
 
     void Follow()
     {
         // 플레이어의 위치로 부드럽게 이동
-        transform.position = Vector3.Lerp(transform.position, player.position, Time.deltaTime);
+        cat.transform.position = Vector3.Lerp(cat.transform.position, player.position, Time.deltaTime);
 
         // 플레이어를 바라보도록 회전
-        Vector3 direction = (player.position - transform.position).normalized; // 플레이어 방향 벡터
+        Vector3 direction = (player.position - cat.transform.position).normalized; // 플레이어 방향 벡터
         if (direction != Vector3.zero) // 방향 벡터가 0이 아닐 때만 회전
         {
             Quaternion lookRotation = Quaternion.LookRotation(direction); // 방향 벡터를 기반으로 회전 생성
-            transform.rotation = Quaternion.Slerp(transform.rotation, lookRotation, Time.deltaTime * 5f); // 부드럽게 회전
+            cat.transform.rotation = Quaternion.Slerp(cat.transform.rotation, lookRotation, Time.deltaTime * 5f); // 부드럽게 회전
         }
     }
 
@@ -82,6 +98,7 @@ public class CATTTTTTTTTTT : MonoBehaviour
         {
             isClose = true;
             text.SetActive(true);
+            Debug.Log("trigger entered");
         }
     }
 
@@ -91,6 +108,7 @@ public class CATTTTTTTTTTT : MonoBehaviour
         {
             isClose = false;
             text.SetActive(false);
+            Debug.Log("trigger exit");
         }
     }
 }
